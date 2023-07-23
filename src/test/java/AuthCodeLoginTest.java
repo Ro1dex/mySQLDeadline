9b001f4ce0ce9efef255b193a1fd25338264b314
Ro1dex <sanhcez58@gmail.com>
2023-07-23 19:04:28 +0200

    update source

--- src/test/java/AuthCodeLoginTest.java
+++ src/test/java/AuthCodeLoginTest.java
@@ -2,9 +2,6 @@ import data.DataHelper;
 import org.junit.jupiter.api.Test;
 import page.LoginPage;
 
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -18,21 +15,5 @@ public class AuthCodeLoginTest {
         var dashboardPage = verificationPage.validVerify();
     }
 
-/*    @Test
-    void shouldTest() {
-        String query = "SELECT code FROM auth_codes WHERE user_id = (SELECT id FROM users WHERE login = vasya)";
-
-        try (
-                var connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass");
-                var getID = connection.prepareStatement(query);
-        ) {
-            ResultSet resultSet = getID.executeQuery();
-            while (resultSet.next()) {
-                String code = resultSet.getString("code");
-            }
-        } catch (SQLException ex) {
-            throw new RuntimeException(ex);
-        }
-    }*/
 }
 
